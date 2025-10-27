61491b8d9dbc965c8b4d3da1b05412369347e057
E_tochka <159701601+e-tochka@users.noreply.github.com>
2025-10-27 21:24:41 +0300

    метрик по прежнему нет, но хочу засейвиться

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# CryptoPic
+# Run
 
-# run
 Для запуска своего скрипта одной командой:
 ```bash
 python src/main.py --mode encrypt --in imgs/checkerboard.png --out checkerboard2.png --algo stream --key qwerty
--- execute_all.py
+++ execute_all.py
@@ -17,26 +17,38 @@ algoritms = [
 key = 'qwerty'
 
 
-
-
-
-
 """
+
+
+|￣￣￣￣￣￣￣|
+  HELP ME PLS |
+|＿＿＿＿＿＿＿|
+     ||  (\__/)
+     ||  (•ㅅ•)
+　＿||ノ ヽ ノ＼＿
+`/　`|| ⌒Ｙ⌒ Ｙ　ヽ
+( 　(三ヽ人　 /　　 |
+|　ﾉ⌒＼ ￣￣ヽ　 ノ
+ヽ＿＿＿＞､＿＿_／
+　　 ｜( 王 ﾉ〈
+　　 /ﾐ`ー―彡
+
+
 Далее программный код, который отделяю, чтобы симитировать var разделение для изменяемый паарметров
 =========================================================================================================
 """
 def init():
     global current_dir 
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    for folder in ['results/encrypted','results/decrypted', 'results/meta']:
+    folders = [
+        'results/encrypted',
+        'results/decrypted', 
+        'results/meta', 
+        'results/metrics'
+    ]
+    for folder in folders:
         full_path = os.path.join(current_dir, folder)
         os.makedirs(full_path, exist_ok=True)
-        
-        gitkeep_file = os.path.join(full_path, '.gitkeep')
-        if not os.path.exists(gitkeep_file):
-            with open(gitkeep_file, 'w') as f:
-                pass
-    
     print("Успешная инициализация")
 
 
@@ -55,25 +67,39 @@ def create_command(_mode, _in, _out, _algo, _key, _iv, _nonce, _meta):
 
 def encrypt(img, algo):
     print(f"Шифруем {img} через {algo} метод...")
-    cmd = create_command('encrypt', 'imgs/'+img, 'results/encrypted/'+img+'_'+algo+'.bin', algo, key, None, None, 'results/meta/'+img+'_'+algo+'.json')
+    cmd = create_command('encrypt', 'imgs/'+img, 'results/encrypted/'+img+'.'+algo+'.bin', algo, key, None, None, 'results/meta/'+img+'.'+algo+'.json')
     execute_cmd(cmd)
 
 def decrypt(img, algo):
     print(f"Дешифруем {img} через {algo} метод...")
-    cmd = create_command('decrypt', 'results/encrypted/'+img+'_'+algo+'.bin', 'results/decrypted/'+img+'_'+algo+'.png', algo, key, None, None, 'results/meta/'+img+'_'+algo+'.json')
+    cmd = create_command('decrypt', 'results/encrypted/'+img+'.'+algo+'.bin', 'results/decrypted/'+img+'.'+algo+'.png', algo, key, None, None, 'results/meta/'+img+'.'+algo+'.json')
     execute_cmd(cmd)
 
 def create_test_images():
+    init()
     for algoritm in algoritms:
         for image in source_images:
             print(f"\n=====({image} by {algoritm})=====")
             encrypt(image, algoritm)
             decrypt(image, algoritm)
 
+"""
+Отделяю блок для метрик
+=========================================================================================================
+"""
+
+def create_metrics():
+    pass
+
+"""
+Отделяю блок "запуска" кода
+=========================================================================================================
+"""
+
 
 def main():
-    init()
     create_test_images()
+    create_metrics()
 
 
 if __name__ == "__main__":
